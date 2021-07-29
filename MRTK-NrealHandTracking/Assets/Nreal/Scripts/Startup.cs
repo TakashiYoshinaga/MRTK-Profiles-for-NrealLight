8a8bee86cac492cb7b6f2650a5e61571612a5bee
Takashi Yoshinaga <yoshinaga.development@gmail.com>
2021-07-29 19:14:23 +0900

    Update Startup.cs

--- MRTK-NrealHandTracking/Assets/Nreal/Scripts/Startup.cs
+++ MRTK-NrealHandTracking/Assets/Nreal/Scripts/Startup.cs
@@ -2,25 +2,31 @@
 //https://hacchi-man.hatenablog.com/entry/2020/04/01/220000
 using UnityEngine;
 using UnityEditor;
+using System.Threading.Tasks;
+
 [InitializeOnLoad]
 public static class Startup
 {
+
     private class StartUpData : ScriptableSingleton<StartUpData>
     {
         [SerializeField]
         private int _callCount;
         public bool IsStartUp()
         {
-            return _callCount++ == 0;
+            return _callCount++ == 1;
         }
     }
-
+    //private static readonly string TMP_FILE_PATH = "Temp/StartupLandmark";
     static Startup()
     {
         if (!StartUpData.instance.IsStartUp())
             return;
         //Force loading of NRSK folder at Unity Editor startup
         string pathRsc = "Assets/NRSDK";
-        AssetDatabase.ImportAsset(pathRsc, ImportAssetOptions.ImportRecursive);
+        AssetDatabase.ImportAsset(pathRsc, ImportAssetOptions.ForceUpdate | ImportAssetOptions.ImportRecursive);
+        Debug.Log("load nrsdk");
+
     }
+   
 }

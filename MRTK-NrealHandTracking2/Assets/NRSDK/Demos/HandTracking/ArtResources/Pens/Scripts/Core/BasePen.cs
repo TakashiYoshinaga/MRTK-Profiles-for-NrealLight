8b927097ff2700c06c06c8c8ecd134237af40da0
Takashi Yoshinaga <yoshinaga.development@gmail.com>
2022-10-07 13:26:31 +0900

    Delete test project

--- MRTK-NrealHandTracking2/Assets/NRSDK/Demos/HandTracking/ArtResources/Pens/Scripts/Core/BasePen.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BasePen : MonoBehaviour, IPen
-{
-    public bool IsDrawing { get; set; }
-    public Transform PenTransform { get { return transform; } }
-}

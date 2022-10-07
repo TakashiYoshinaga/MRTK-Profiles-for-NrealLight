3a22af14564fc2d907da8489a912613362e128a3
Takashi Yoshinaga <yoshinaga.development@gmail.com>
2022-10-07 13:26:31 +0900

    Delete test project

--- MRTK-NrealHandTracking2/Assets/NRSDK/Demos/HandTracking/ArtResources/Pens/Scripts/Core/IPen.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IPen
-{
-    bool IsDrawing { get; set; }
-    Transform PenTransform { get; }
-}
